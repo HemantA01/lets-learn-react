562670999a6f028c2ce82bb9be4572710d3fc179
HemantA01 <kumarhemant22@gmail.com>
2026-08-22 13:21:55 +0530

    Counter Project with addition of values

---
+++ 02-counter/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'    //Here is the point where we are calling Hooks (useState, useEffect, useCallback etc.)
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  //let counter = 10;
+  let [counter, setCounter] = useState(10);
+  const addValue = () => {
+    console.log('Add value button clicked. Counter is: ', counter)
+    counter += 2
+    setCounter(counter)
+  }
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <button type="button" className="counter">
+          Count is {counter}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+        
+      <section id="next-steps">
+        <div id="docs">
+          <button type="button" className="counter" onClick={addValue}>Increase Value</button>  
+        </div>
+        <div id="social">
+          <button type="button" className="counter">Decrease Value</button>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
